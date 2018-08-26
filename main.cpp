de9f3c3362ac94b6d7f1609a79dc7f1c261b3794
Pat Lorton <pat.lorton@schrodinger.com>
2018-08-26 10:08:59 -0400

    Add check for incompatible options

--- main.cpp
+++ main.cpp
@@ -30,6 +30,20 @@ int main(int argc, char* argv[])
         return 1;
     }
     
+    if(parser.isSet(helpOption)) {
+        qDebug() << "Arg parsing is only done in a reasonable way in"
+            " the python gpusim_server.py.  Handling here is very error prone"
+            " and not intended for direct use.";
+        return 1;
+    }
+
+    bool ok = false;
+    auto gpu_bitcount = parser.value(gpuBitcountOption).toInt(&ok);
+
+    if(parser.isSet(cpuOnlyOption) && parser.isSet(gpuBitcountOption)) {
+        qDebug() << "--cpu_only and --gpu_bitcount are incompatible options";
+        return 1;
+    }
 
     auto db_fnames =  parser.positionalArguments();
     for(auto filename : db_fnames) {
@@ -40,19 +54,12 @@ int main(int argc, char* argv[])
         }
     }
 
-    bool ok = false;
-    auto gpu_bitcount = parser.value(gpuBitcountOption).toInt(&ok);
     if(!ok) {
         qDebug() << "GPU Bitcount must be an integer";
         return 1;
     }
 
-    if(parser.isSet(helpOption)) {
-        qDebug() << "Arg parsing is only done in a reasonable way in"
-            " the python gpusim_server.py.  Handling here is very error prone"
-            " and not intended for direct use.";
-        return 1;
-    }
+
     GPUSimServer gpusim(db_fnames, gpu_bitcount);
     gpusim.setUseGPU(!parser.isSet(cpuOnlyOption));
 
