cf7a5399f78a3b64d50149d438a8741a3616a370
Steffen Christgau <christgau@zib.de>
2024-01-23 14:45:24 +0100

    Enable compatibility with C++14 and Qt 5.14
    - Conditionally use back-ported template specialization
    - Replace depcreated timer class
    - Bump required language standard to C++14 to avoid warning (errors)
      with more recent CUDA toolkits than CUDA 9 (caused by Thrust)

--- qstring_hash.h
+++ qstring_hash.h
@@ -2,9 +2,13 @@
 #include <QString>
 #include <functional>
 
+#if QT_VERSION < QT_VERSION_CHECK(5, 14, 0)
+
 namespace std
 {
 template <> struct hash<QString> {
     std::size_t operator()(const QString& s) const { return qHash(s); }
 };
 } // namespace std
+
+#endif /* QT_VERSION */
